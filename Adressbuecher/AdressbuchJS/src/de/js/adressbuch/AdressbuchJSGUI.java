8921513674a76b56e400777a0c0b0af36589a7db
zian92 <xyxyxyz@hotmail.de>
2014-02-19 20:59:45 +0100

    Adressbuch grid working, no data

---
+++ Adressbuecher/AdressbuchJS/src/de/js/adressbuch/AdressbuchJSGUI.java
@@ -0,0 +1,40 @@
+package de.js.adressbuch;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class AdressbuchJSGUI {
+	public static void main(String args[]) {
+		Object rows[][] = {  {"","","","","","","","","",""},{"","","","","","","","","",""},{"","","","","","","","","",""},{"","","","","","","","","",""},{"","","","","","","","","",""},{"","","","","","","","","",""},{"","","","","","","","","",""},{"","","","","","","","","",""},{"","","","","","","","","",""},{"","","","","","","","","",""},};
+		Object headers[] = { "Vorname", "Anrede", "Nachname", "Strasse", "PLZ", "Ort", "Telefon", "Handy", "EMail", "Land" };
+		JTable table = new JTable(rows, headers);
+		JScrollPane scrollPane = new JScrollPane(table);
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(scrollPane, BorderLayout.CENTER);
+		frame.setSize(600, 150);
+		frame.setVisible(true);
+	}
+}
+
+@SuppressWarnings("serial")
+class HeaderlessTable extends JTable {
+	public HeaderlessTable(Object rowData[][], Object columnNames[]) {
+		super(rowData, columnNames);
+	}
+
+	protected void configureEnclosingScrollPane() {
+		Container container = getParent();
+		if (container instanceof JViewport) {
+			Container viewParent = container.getParent();
+			if (viewParent instanceof JScrollPane) {
+				JScrollPane scrollPane = (JScrollPane) viewParent;
+				JViewport viewport = scrollPane.getViewport();
+				if (viewport == null || viewport.getView() != this) {
+					return;
+				}
+				scrollPane.setBorder(UIManager.getBorder("Table.scrollPaneBorder"));
+			}
+		}
+	}
+}
