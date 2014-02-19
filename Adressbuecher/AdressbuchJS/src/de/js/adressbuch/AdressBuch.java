8376d111c59877e7bb58cb7d5404559bfa3f0233
zian92 <xyxyxyz@hotmail.de>
2014-02-19 20:41:07 +0100

    adressbuch-implementierung

--- Adressbuecher/AdressbuchJS/src/de/js/adressbuch/AdressBuch.java
+++ Adressbuecher/AdressbuchJS/src/de/js/adressbuch/AdressBuch.java
@@ -1,17 +1,62 @@
 package de.js.adressbuch;
 
+import java.util.ArrayList;
+
+/**
+ * repraesentiert ein Adressbuch.
+ * @author Jonas
+ *
+ */
 public class AdressBuch {
 
+	private ArrayList<AdressEintrag> ab;
+
+	/**
+	 * erstellt ein leeres Adressbuch.
+	 */
 	public AdressBuch() {
-		// TODO Auto-generated constructor stub
+		ab = new ArrayList<AdressEintrag>();
 	}
 
 	/**
-	 * @param args
+	 * fuegt dem Adressbuch einen Eintrag hinzu.
+	 * @param e
 	 */
-	public static void main(String[] args) {
-		AdressEintrag a = new AdressEintrag("lkj", "lkj", Anrede.getString(Anrede.DR));
-		System.out.println(a.getAnrede());
+	public void addEintrag(AdressEintrag e){
+		ab.add(e);
 	}
+	
+	/**
+	 * loescht einen Eintrag aus dem Adressbuch anhand des Objektes.
+	 * @param e Objekt des Eintrags.
+	 * @return <i>true</i> wenn der Eintrag sich in der Liste befunden hat.
+	 */
+	public boolean deleteEintrag(AdressEintrag e){
+		return ab.remove(e);
+	}
+	
+	/**
+	 * loescht einen Eintrag aus dem Adressbuch anhand der Stelle des Eintrags.
+	 * @param index stelle des Eintrags
+	 * @return <i>true</i> wenn der Eintrag sich in der Liste befunden hat.
+	 */
+	public boolean deleteEintrag(int index){
+		return ab.remove(index) != null;
+	}
+	
+	/**
+//	 * @param args
+//	 */
+//	public static void main(String[] args) {
+//		AdressEintrag a = new AdressEintrag("lkj", "lkj", Anrede.getString(Anrede.DR));
+//		System.out.println(a.getAnrede());
+//	}
 
+	/**
+	 * 
+	 * @return
+	 */
+	public ArrayList<AdressEintrag> getAb() {
+		return ab;
+	}
 }
