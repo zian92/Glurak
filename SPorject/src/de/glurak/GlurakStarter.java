d74d3369777632580a9b8049b091b7d7338646fa
zian92 <xyxyxyz@hotmail.de>
2014-02-24 16:29:50 +0100

    JAVA Project initial

---
+++ SPorject/src/de/glurak/GlurakStarter.java
@@ -0,0 +1,16 @@
+package de.glurak;
+
+public class GlurakStarter {
+
+	public GlurakStarter() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Dis is Gluwrak");
+	}
+
+}
