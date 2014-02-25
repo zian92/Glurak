79b3862e16d5f5e94a3a444d629e48c81323c11e
Simon Wolter <simon-wolter@web.de>
2014-02-25 15:42:08 +0100

    Java Klasse: Message

---
+++ SProject/src/de/glurak/data/Message.java
@@ -0,0 +1,37 @@
+package de.glurak.data;
+
+public class Message {
+
+	private String message;
+	private User sender;
+	private User receiver;
+	
+	public Message(){
+		
+	}
+	
+	public void setMessage(String pMessage){
+		
+	}
+	
+	public void setSender(User pSender){
+		
+	}
+	
+	public void setReceiver(User pReceiver){
+		
+	}
+	
+	public String getMessage(){
+		return message;
+	}
+	
+	public User getSender(){
+		return sender;
+	}
+	
+	public User getReceiver(){
+		return receiver;
+	}
+	
+}
