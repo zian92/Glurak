41dad46d8d6508ba6878c75a8715cf33de695e85
zian92 <xyxyxyz@hotmail.de>
2014-02-25 16:21:34 +0100

    Anwendungsquery klasse hinzugefügt

---
+++ SProject/src/de/glurak/Query.java
@@ -0,0 +1,16 @@
+package de.glurak;
+
+public class Query {
+
+	private final static String applicationName = "Gluwrak rules the world";
+	private final static String rootPath = "./";
+
+	public static String getRootPath() {
+		return rootPath;
+	}
+
+	public static String getApplicationName() {
+		return applicationName;
+	}
+
+}
