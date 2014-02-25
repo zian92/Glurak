57fcd52da1d0693db9ab0fb7e18faea40ce9f76b
zian92 <xyxyxyz@hotmail.de>
2014-02-25 09:54:08 +0100

    AdressbuchJS fertig

--- Adressbuecher/AdressbuchJS/src/de/js/adressbuch/AdressbuchJSGUI.java
+++ Adressbuecher/AdressbuchJS/src/de/js/adressbuch/AdressbuchJSGUI.java
@@ -3,16 +3,20 @@ package de.js.adressbuch;
 import java.awt.BorderLayout;
 import java.awt.Button;
 import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.Panel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Vector;
 
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
@@ -29,19 +33,24 @@ public class AdressbuchJSGUI implements Serializable {
 	private static final long serialVersionUID = 1L;
 	// initalisierung Variablen
 	protected final Object headers[] = { "Vorname", "Anrede", "Nachname", "Strasse", "PLZ", "Ort", "Telefon", "Handy", "EMail", "Land" };
-	protected String anwendungsname = "Adressbuch JS";
+	protected final String anwendungsname = "Adressbuch JS";
 	protected JTable table;
-	protected String path = "./Adressbuch.ser";
-	protected String[][] emptyRow = new String[][] { { "", "", "", "", "", "", "", "", "", "" }, };
+	protected final String path = "./Adressbuch.ser";
+	protected final Object[] emptyRow = new Object[] { "", "", "", "", "", "", "", "", "", "" };
+	protected final Object[][] emptyRows = new String[][] { { "", "", "", "", "", "", "", "", "", "" }, };
 
 	public static void main(String args[]) {
+		@SuppressWarnings("unused")
 		AdressbuchJSGUI js = new AdressbuchJSGUI();
 
 	}
 
+	/**
+	 * Erstellt Die GUI mit allen Elementen
+	 */
 	public AdressbuchJSGUI() {
 		// Daten Laden
-		Object rows[][] = (Object[][]) this.toStringArray();
+		Object rows[][] = this.loadAdressbuch();
 
 		// initialisierung GUI
 		table = new JTable(new DefaultTableModel(rows, headers));
@@ -49,108 +58,128 @@ public class AdressbuchJSGUI implements Serializable {
 		JFrame frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.add(scrollPane, BorderLayout.CENTER);
-		Button b = new Button("Speichern");
-		frame.add(b, BorderLayout.SOUTH);
 
+		// Buttons adden
+		Button b = new Button("Speichern");
 		b.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				DefaultTableModel model = (DefaultTableModel) table.getModel();
-				model.addRow(new Object[] { "", "", "", "", "", "", "", "", "", "" });
 				saveAdressbuch();
 			}
 		});
 
+		Button b1 = new Button("Beenden");
+		b1.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+
+		Button b2 = new Button("Neue Zeile");
+		b2.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				addRow();
+			}
+		});
+		Button b3 = new Button("Markierte Zeile loeschen");
+		b3.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				deleteRow();
+			}
+		});
+		Panel p = new Panel(new FlowLayout());
+
+		p.add(b2);
+		p.add(b3);
+		p.add(b);
+		p.add(b1);
+
+		frame.add(p, BorderLayout.SOUTH);
 		frame.setSize(600, 500);
 		frame.setTitle(anwendungsname);
 		frame.setVisible(true);
 	}
 
-	public String[][] toStringArray() {
+	/**
+	 * Gibt den Tabelleninhalt wieder.
+	 * 
+	 * @return String[][]
+	 */
+	public String[][] getTableData() {
+		String[][] data = new String[10][table.getRowCount()];
+		for (int i = 0; i < table.getRowCount(); i++)
+			for (int j = 0; j < table.getColumnCount(); j++)
+				data[i][j] = (String) table.getValueAt(i, j);
+		return data;
+	}
+
+	/**
+	 * Loescht die ausgewählte Tabelle
+	 */
+	public void deleteRow() {
+		int i = table.getSelectedRow();
+		if (i >= 0 && i < table.getRowCount()) {
+			((DefaultTableModel) table.getModel()).removeRow(i);
+		}
+	}
+
+	/**
+	 * Fuegt am ende eine Zeile hinzu
+	 */
+	public void addRow() {
+		((DefaultTableModel) table.getModel()).addRow(emptyRow);
+	}
+
+	/**
+	 * laedt das Adressbuch aus dem speicher, falls möglich
+	 * 
+	 * @return
+	 */
+	@SuppressWarnings("resource")
+	public String[][] loadAdressbuch() {
+		String[][] data = null;
+		InputStream fis = null;
 		try {
-			AdressEintrag[] a = (AdressEintrag[]) this.readAdressbuch().toArray();
-			return convertToString2DArray(a);
-		} catch (Exception e) {
-			return emptyRow;
-		}
-	}
-
-	public String[][] convertToString2DArray(AdressEintrag[] a) {
-		String[][] s = new String[10][a.length];
-		for (int i = 0; i < a.length; i++) {
-			String[] temp = a[i].toStringArray();
-			for (int j = 0; j < 10; j++) {
-				s[j][i] = temp[j];
-			}
-		}
-		return s;
-	}
-
-	public AdressEintrag[] getEintraege() {
-		AdressEintrag[] a = new AdressEintrag[table.getRowCount()];
-		for (int i = 0; i < a.length; i++) {
-			a[i] = new AdressEintrag(table.getModel().getValueAt(i, 0).toString(), table.getModel().getValueAt(i, 1).toString(), table.getModel().getValueAt(i, 2).toString(), table.getModel().getValueAt(i, 3).toString(), table.getModel().getValueAt(i, 4).toString(), table.getModel().getValueAt(i, 5).toString(), table.getModel().getValueAt(i, 6).toString(), table.getModel().getValueAt(i, 7).toString(), table.getModel().getValueAt(i, 8).toString(), table.getModel().getValueAt(i, 9).toString());
-		}
-		return a;
-	}
-
-	private ArrayList<AdressEintrag> getArrayListFromArray(AdressEintrag[] ae) {
-		ArrayList<AdressEintrag> temp = new ArrayList<AdressEintrag>();
-		for (AdressEintrag a : temp) {
-			temp.add(a);
-		}
-		return temp;
-	}
-
-	public void saveAdressbuch() {
-		this.checkRows();
-		ArrayList<AdressEintrag> ae = this.getArrayListFromArray(this.getEintraege());
-		try {
-			// Serialize data object to a file
-			ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(path));
-			out.writeObject(ae);
-			out.close();
+			fis = new FileInputStream(path);
+			ObjectInputStream ois = new ObjectInputStream(fis);
+			data = (String[][]) ois.readObject();
+		} catch (FileNotFoundException e) {
+			System.out.println("Keine Adressdaten vorhanden.");
+			System.out.println("Lege neue Datei an...");
+			data = (String[][]) emptyRows;
 		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
 		}
+		return data;
 	}
 
-	public ArrayList<AdressEintrag> readAdressbuch() {
-		ArrayList<AdressEintrag> a = new ArrayList<AdressEintrag>();
-		ObjectInputStream objIn = null;
-
+	/**
+	 * speichert das Adressbuch lokal.
+	 */
+	public void saveAdressbuch() {
+		String[][] data = this.getTableData();
+		FileOutputStream fos = null;
+		ObjectOutputStream oos;
 		try {
-			objIn = new ObjectInputStream(new FileInputStream(path));
+			fos = new FileOutputStream(path);
+			oos = new ObjectOutputStream(fos);
+			oos.writeObject(data);
+		} catch (FileNotFoundException fosException) {
+			fosException.printStackTrace();
+		} catch (IOException oosException) {
+			oosException.printStackTrace();
+		} finally {
 			try {
-				while (true) {
-					a.add((AdressEintrag) objIn.readObject());
-				}
-			} catch (Exception e) {
-			}
-			objIn.close();
-		} catch (FileNotFoundException e2) {
-			// e2.printStackTrace();
-		} catch (IOException e2) {
-			// e2.printStackTrace();
-		}
-		return a;
-	}
-
-	public void checkRows() {
-		AdressEintrag[] a = this.getEintraege();
-		ArrayList<AdressEintrag> al = this.getArrayListFromArray(a);
-
-		int rowcount = a.length;
-		for (int i = a.length; i <= 0; i--) {
-			if (table.getModel().getValueAt(i, 0).toString() == "" & table.getModel().getValueAt(i, 1).toString() == "" & table.getModel().getValueAt(i, 2).toString() == "" & table.getModel().getValueAt(i, 3).toString() == "" & table.getModel().getValueAt(i, 4).toString() == "" & table.getModel().getValueAt(i, 5).toString() == "" & table.getModel().getValueAt(i, 6).toString() == "" & table.getModel().getValueAt(i, 7).toString() == "" & table.getModel().getValueAt(i, 8).toString() == ""
-					& table.getModel().getValueAt(i, 9).toString() == "") {
-				al.remove(i);
+				fos.close();
+			} catch (IOException e1) {
+				e1.printStackTrace();
 			}
 		}
-		if (a.length == rowcount) {
-			al.add(new AdressEintrag());
-		}
-		String[][] temp = this.convertToString2DArray((AdressEintrag[]) al.toArray());
-		table = new JTable(new DefaultTableModel(temp, headers));
 	}
 }
 
