9305622e7668a18e8e1a2df5b009c215aa054b9b
Christopher-Di <christopher.distelkaemper@t-online.de>
2014-02-26 11:19:17 +0100

    Rechtschreibfehler Query

--- SProject/src/de/glurak/Query.java
+++ SProject/src/de/glurak/Query.java
@@ -2,7 +2,7 @@ package de.glurak;
 
 public class Query {
 
-	public final static String APPLICATION_NAME = "Gluwrak rules the world";
+	public final static String APPLICATION_NAME = "Glurak rules the world";
 	public final static String ROOTPATH = "./";
 
 }
