9667bc86a1ced5c6ca2418db4078c1896ef504fe
Simon Wolter <simon-wolter@web.de>
2014-02-26 11:38:48 +0100

    RegistrationView Update

--- SProject/src/de/glurak/frontend/registration/RegistrationView.java
+++ SProject/src/de/glurak/frontend/registration/RegistrationView.java
@@ -1,5 +1,20 @@
 package de.glurak.frontend.registration;
 
-public class RegistrationView {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
+/**
+ * In der RegistrationView kann ein Benutzer des System sich einen neuen Account anlegen.
+ * Um sich zu registrieren muss man die geforderten Daten angeben.
+ * @author Simon
+ *
+ */
+public class RegistrationView extends JFrame{
+
+	public RegistrationView(String pTitel){
+		super(pTitel);	
+	}
+	
 }
