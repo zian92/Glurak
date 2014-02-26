7e6db797c7ed5a0e560aaf9c642ece69a7a16b37
Christopher-Di <christopher.distelkaemper@t-online.de>
2014-02-26 14:22:24 +0100

    UPDATE ProfileView

--- SProject/src/de/glurak/frontend/mainFrame/content/profile/ProfileView.java
+++ SProject/src/de/glurak/frontend/mainFrame/content/profile/ProfileView.java
@@ -1,5 +1,14 @@
 package de.glurak.frontend.mainFrame.content.profile;
 
+import javax.swing.*;
+
+/**
+ * Die ProfileView zeigt dem User ein Profil an.
+ * @author Christopher Distelkämper
+ *
+ */
+
 public class ProfileView {
 
+	// 
 }
