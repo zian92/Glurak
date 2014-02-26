78de66fc5b3f9f6cde7353f2004bef0d1ef67854
Christopher-Di <christopher.distelkaemper@t-online.de>
2014-02-26 15:11:40 +0100

    UPDATE ProfileView

--- SProject/src/de/glurak/frontend/mainFrame/content/profile/ProfileView.java
+++ SProject/src/de/glurak/frontend/mainFrame/content/profile/ProfileView.java
@@ -2,6 +2,8 @@ package de.glurak.frontend.mainFrame.content.profile;
 
 import javax.swing.*;
 
+import java.awt.*;
+
 /**
  * Die ProfileView zeigt dem User ein Profil an.
  * @author Christopher Distelkämper
@@ -10,5 +12,71 @@ import javax.swing.*;
 
 public class ProfileView {
 
-	// 
+	// Panels
+	private JPanel pan_profileview;
+	private JPanel pan_profilepic;
+	private JPanel pan_profiledata;
+	private JPanel pan_topplaylists;
+	private JPanel pan_likes;
+	
+	// Buttons
+	private JButton b_message;
+	private JButton b_follow;
+	private JButton b_moreplaylists;
+	
+	/**
+	 * Constructor
+	 */
+	public ProfileView(){
+		
+		// Initialisieren Panel pan_profileview
+		pan_profileview = new JPanel(new GridBagLayout());
+		pan_profileview.setSize(800,500);
+		
+		// Layout-Restriktionen festlegen.
+		GridBagConstraints c = new GridBagConstraints();
+		c.fill = GridBagConstraints.BOTH;
+		c.insets = new Insets(1,1,2,2);	
+		
+		// Initialisieren Panel pan_likes
+		pan_likes = new JPanel();
+		
+		// Initialisieren Panel pan_profilepic
+		pan_profilepic = new JPanel();
+		
+		   // Initialisieren der Buttons b_message und b_follow
+		   b_message = new JButton("Nachricht");
+		   pan_profilepic.add(b_message);
+		   b_follow = new JButton("Follow");
+		   pan_profilepic.add(b_follow);
+		
+		// Initialisieren Panel pan_topplaylists
+		pan_topplaylists = new JPanel();	
+		
+		// Initialisieren Panel pan_profiledata
+		pan_profiledata = new JPanel();			
+		
+		// Hinzufügen der Panels zum Panel pan_profileview
+		c.gridx = 0;
+		c.gridy = 0;
+		pan_profileview.add(pan_likes, c);
+		
+		c.gridx = 0;
+		c.gridy = 1;
+		pan_profileview.add(pan_profilepic, c);
+		
+		c.gridx = 1;
+		c.gridy = 0;
+		pan_profileview.add(pan_topplaylists);
+		
+		c.gridx = 1;
+		c.gridy = 1;
+		pan_profileview.add(pan_profiledata);
+		
+		 
+		
+		
+		
+		
+	}
 }
