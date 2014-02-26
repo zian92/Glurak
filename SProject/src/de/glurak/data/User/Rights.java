af7206e5044fb4972f4b9264dfebd0e174a32127
Entscheider <noone@nowhere.org>
2014-02-26 14:14:28 +0100

    Hibernate in Entitnitätsklassen und User überarbeitet

---
+++ SProject/src/de/glurak/data/User/Rights.java
@@ -0,0 +1,9 @@
+package de.glurak.data.User;
+
+/**
+ * Hier sind alle Rechte gesammelt die es gibt...
+ * @author Entscheider
+ */
+public class Rights {
+    public final static String[] LISTENER_RIGHTS={};
+}
---
+++ SProject/src/de/glurak/data/User/UserProfile.java
@@ -0,0 +1,55 @@
+package de.glurak.data.User;
+
+import java.io.Serializable;
+import javax.persistence.*;
+/**
+ * Oberklasse aller Profile eines Benutzers
+ * @author Entscheider
+ */
+public abstract class UserProfile extends Profile implements Serializable {
+    public abstract String[] myRights();
+    @Id
+    @GeneratedValue
+    private long id;
+    protected String email;
+    protected String firstname;
+    protected String lastname;
+    protected String emailadr;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmailadr() {
+        return emailadr;
+    }
+
+    public void setEmailadr(String emailadr) {
+        this.emailadr = emailadr;
+    }
+
+    public long getId(){
+        return id;
+    }
+
+}
