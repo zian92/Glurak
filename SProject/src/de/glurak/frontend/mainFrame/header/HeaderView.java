a01922928bc96ffd973315d2074fc45be3b21659
Christopher-Di <christopher.distelkaemper@t-online.de>
2014-02-27 14:28:53 +0100

    UPDATE HeaderView

--- SProject/src/de/glurak/frontend/mainFrame/header/HeaderView.java
+++ SProject/src/de/glurak/frontend/mainFrame/header/HeaderView.java
@@ -5,15 +5,15 @@ import java.awt.*;
 
 /**
  * Die HeaderView ist dazu da die das oberste Panel anzuzeigen.
- * Dieses enthält unter anderem die Suchefunktion.
- * @author Christopher Distelkämper
+ * Dieses enthÃ¤lt unter anderem die Suchefunktion.
+ * @author Christopher DistelkÃ¤mper
  * Date: 26.02.2014
  */
-public class HeaderView extends JFrame{
+public class HeaderView extends JPanel{
 	
-	// Textfeld für die Sucheingabe
+	// Textfeld fÃ¼r die Sucheingabe
 	private JTextField t_search;
-	// Button für die Sucheingabe 
+	// Button fÃ¼r die Sucheingabe 
 	private JButton b_search;
 	// Panel des Headers
 	private JPanel pan_header;	
@@ -35,24 +35,54 @@ public class HeaderView extends JFrame{
 		// Initialisierung des Textfeldes
 		c.weightx = 1.0;
 		c.gridx = 0;
-		c.gridy = 5;
+		c.gridy = 0;
 		t_search = new JTextField();
 		t_search.setText("Sucheingabe");
 		pan_header.add(t_search, c);
 		
 		// Initialisierung des Suchbuttons
-		c.gridx = 0;
-		c.gridy = 6;
+		c.gridx = 1;
+		c.gridy = 0;
 		b_search = new JButton("Suchen");
 		pan_header.add(b_search, c);
 		
-		// Beschaffen der ContentPane
-		java.awt.Container content = getContentPane();
-		
-		// Hinzufügen des Panels zur ContentPane
-		content.add(pan_header);		
+		// HinzufÃ¼gen des Panels zur ContentPane
+		add(pan_header);		
 		setVisible(true);
 		
 	}
 
+	/**
+	 * Erzeugt die ProfileView und zeigt sie an.
+	 */
+	private static void createAndShowView(){
+		//Erzeugen des Frames
+		JFrame header = new JFrame("Header");
+		header.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		//Registrationview in das Frame laden
+		JComponent newContentPane = new HeaderView();
+        newContentPane.setOpaque(true);
+        header.setContentPane(newContentPane);
+        
+        //Groesse des Frames festlegen
+        header.setPreferredSize(new Dimension(1000, 500));
+        //Groesse des Frames soll nicht veraenderbar sein
+        header.setResizable(false);
+        //Registrationview wird in der Mitte des Bildschirms geladen
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        header.setLocation(dim.width/2-header.getSize().width/2-300, dim.height/2-header.getSize().height/2-150);
+		
+		//Frame anpassen und sichtbar machen
+		header.pack();
+		header.setVisible(true);
+	}
+	
+	public static void main(String[] args){
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowView();
+            }
+        });
+	}
 }
