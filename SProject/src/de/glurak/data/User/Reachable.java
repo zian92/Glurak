587b893aa55f3cc1d4a03ba479aa29bdd960888d
dadomadi <darkodermadi@gmx.de>
2014-02-28 12:21:17 +0100

    Reachable implementiert Hateable

--- SProject/src/de/glurak/data/User/Reachable.java
+++ SProject/src/de/glurak/data/User/Reachable.java
@@ -4,7 +4,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
+
+import de.glurak.data.Hateable;
+
 import java.io.Serializable;
+import java.util.List;
 
 /**
  * Oberklasse für alles im Programm,
@@ -12,8 +16,9 @@ import java.io.Serializable;
  * @author Entscheider
  */
 @Entity
-abstract public class Reachable  implements Serializable{
-    @Id
+abstract public class Reachable  implements Serializable, Hateable{
+   
+	@Id
     @GeneratedValue
     private long id;
 
@@ -21,4 +26,42 @@ abstract public class Reachable  implements Serializable{
     abstract public Profile getProfile();
 
     public long getId(){return id;}
+    
+    /**
+	 * Gibt einen Negativpunkt im Hate-Count.
+	 * @param hater der User, der negativ bewertet hat.
+	 */
+	public void hate(User hater);
+	
+	/**
+	 * Gibt einen Positivpunkt im Like-Count.
+	 * @param Liker der User, der positiv bewertet hat.
+	 */
+	public void like(User Liker);
+	
+	/**
+	 * Inkrementiert den Hate-Count.
+	 * @return Hate-Count
+	 */
+	public int hateCount();
+	
+	/**
+	 * Inkrementiert den Like-Count.
+	 * @return Like-Count
+	 */
+	public int likeCount();
+	
+	/**
+	 * Gibt die Liste aller Hater zurück
+	 * @return Liste alle Hater
+	 */
+	public List<User> getHater();
+	
+	/**
+	 * Gibt die Liste aller Liker zurück
+	 * @return Liste aller Liker
+	 */
+	public List<User> getLiker();
+
+    
 }
