0b1ae6ac2e1f743efd4ef5b1bb6db64cbdd07da3
zian92 <xyxyxyz@hotmail.de>
2014-03-05 15:08:09 +0100

    start bereinigt

--- SProject/src/de/glurak/GlurakStarter.java
+++ SProject/src/de/glurak/GlurakStarter.java
@@ -1,7 +1,5 @@
 package de.glurak;
 
-import java.awt.Component;
-
 import javax.swing.SwingUtilities;
 
 import de.glurak.frontend.login.LoginVController;
@@ -9,25 +7,14 @@ import de.glurak.frontend.login.LoginVController;
 public class GlurakStarter {
 
     public GlurakStarter() {
-        // TODO Auto-generated constructor stub
     }
 
-    /**
-     * @param args
-     */
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 // Create GUI
                 LoginVController logControll = new LoginVController(Query.APPLICATION_NAME);
-                // LoginView frame_Login = new LoginView(Query.APPLICATION_NAME);
-                // JFrame frame = new JFrame();
-                // logControll.getView().setSize(1024, 700);
             }
         });
-
-    }
-
-    public void test(Component comp) {
     }
 }
