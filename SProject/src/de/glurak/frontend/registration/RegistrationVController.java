382bf86c30c4746db67d864d2cad7c8f785c7831
Simon Wolter <simon-wolter@web.de>
2014-03-05 10:51:01 +0100

    RegistrationVController richtig

--- SProject/src/de/glurak/frontend/registration/RegistrationVController.java
+++ SProject/src/de/glurak/frontend/registration/RegistrationVController.java
@@ -29,45 +29,6 @@ public class RegistrationVController implements ActionListener{
 	public void actionPerformed(ActionEvent e){
 		if (e.getActionCommand().equals("registrate")){
 			try {
-<<<<<<< HEAD
-				int day = Integer.parseInt(regview.t_birthdate_day.getText());
-				int month = Integer.parseInt(regview.t_birthdate_month.getText());
-				int year = Integer.parseInt(regview.t_birthdate_year.getText());
-				//Abfrage, ob alle Textfelder ausgefüllt wurden 
-				if (	(regview.t_username.getText() == null) ||
-						(regview.t_password.getPassword().length == 0) ||
-						(regview.t_birthdate_day.getText() == null) ||
-						(regview.t_birthdate_month.getText() == null) ||
-						(regview.t_birthdate_year.getText() == null) ||
-						(regview.d_homecountry.getSelectedIndex() == 0) ||
-						(regview.r_gender_f.isSelected() == false && regview.r_gender_m.isSelected() == false)){
-					JOptionPane.showMessageDialog(null, "Bitte füllen sie alle Felder aus, um sich zu registrieren!", "Fehlermeldung", JOptionPane.ERROR_MESSAGE);	
-				}
-				else{
-					//Abfrage, ob es ein gueltiges Geburtstagsdatum ist
-					if (	( day > 31) ||
-							(month > 12) ||
-							(year > 2014)){
-						JOptionPane.showMessageDialog(null, "Bitte geben sie ein richtiges Geburtsdatum an!", "Fehlermeldung", JOptionPane.ERROR_MESSAGE);	
-					}
-					else{
-						//Abfrage, ob es den Usernamen bereits gibt
-						//TODO Usernamen mit Datenbankeintraegen vergleichen
-						if(regview.t_username.getText() == "!!!Platzhalter!!!"){
-							JOptionPane.showMessageDialog(null, "Dieser Username ist bereits belegt. Bitte suchen sie sich einen annderen Usernamen aus!", "Fehlermeldung", JOptionPane.ERROR_MESSAGE);	
-						}
-						else{
-							//TODO User in der Datenbank registrieren
-							/**
-							 * Was soll passieren wenn man den register Button drueckt
-							 */
-							JOptionPane.showMessageDialog(null, "Sie haben sich erfolgreich registriert! Sie können sich nun mit ihren Account einloggen.", "Registrierung erfolgreich!", JOptionPane.PLAIN_MESSAGE);	
-						}
-					}
-				}
-			}
-=======
-
                 User u = regview.getEnteredUser();
                 if (u==null){
                     JOptionPane.showMessageDialog(regview, "Bitte füllen sie alle Felder korrekt aus, um sich zu registrieren!", "Fehlermeldung", JOptionPane.ERROR_MESSAGE);
@@ -88,7 +49,6 @@ public class RegistrationVController implements ActionListener{
 
                 regview.setVisible(false);
             }
->>>>>>> 06343dfaab655baf11127857ed65766869f96f19
 			catch (NumberFormatException nmf){
 				JOptionPane.showMessageDialog(regview, "Bitte geben sie ein gültiges Datum an!", "Fehlermeldung", JOptionPane.ERROR_MESSAGE);
 			}
