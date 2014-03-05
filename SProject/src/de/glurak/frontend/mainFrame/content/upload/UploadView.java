a0e4cd13d0424b9efe4167f5a12bb51da7393571
Simon Wolter <simon-wolter@web.de>
2014-03-05 10:48:17 +0100

    UploadView angefangen

--- SProject/src/de/glurak/frontend/mainFrame/content/upload/UploadView.java
+++ SProject/src/de/glurak/frontend/mainFrame/content/upload/UploadView.java
@@ -1,5 +1,30 @@
 package de.glurak.frontend.mainFrame.content.upload;
 
-public class UploadView {
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
+/**
+ * In der Uploadview kann man Medien auswaehlen, die man hochladen moechte
+ * @author Simon
+ *
+ */
+public class UploadView extends JPanel {
+
+	//Buttons
+	protected JButton b_upload;
+	protected JButton b_cancel;
+	//Textfelder
+	protected JTextField t_artist;
+	protected JTextField t_title;
+	protected JTextField t_album;
+	
+	/**
+	 * Konstruktor
+	 */
+	public UploadView(){
+		
+	}
+	
 }
