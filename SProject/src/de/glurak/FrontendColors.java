d68d594c0b5dde5f3b9b980eacde874263712b34
Maxim <mxb.dance@gmx.de>
2014-03-06 15:15:31 +0100

    Colors and Playlist in database

---
+++ SProject/src/de/glurak/FrontendColors.java
@@ -0,0 +1,12 @@
+package de.glurak;
+
+import java.awt.Color;
+
+public class FrontendColors {
+	
+	public static final Color DARK_GREY = new Color(4210752);
+	
+	private FrontendColors(){
+		
+	}
+}
--- SProject/src/de/glurak/frontend/mainFrame/content/playlist/PlaylistVController.java
+++ SProject/src/de/glurak/frontend/mainFrame/content/playlist/PlaylistVController.java
@@ -11,6 +11,7 @@ import javax.swing.JLabel;
 import javax.swing.JTable;
 
 import de.glurak.data.Playlist;
+import de.glurak.frontend.SessionThing;
 import de.glurak.frontend.mainFrame.ContentController;
 
 public class PlaylistVController extends Observable implements MouseListener, ActionListener, ContentController {
@@ -42,10 +43,14 @@ public class PlaylistVController extends Observable implements MouseListener, Ac
     	}else if (e.getActionCommand().equals("prevSlide")){
     		view.prevPage();
     	}else if(e.getActionCommand().equals("newList")){
-    		Playlist p = new Playlist(5, "Pokemon");
+    		Playlist p = new Playlist();
+    		p.setName("Pokemon");
+    		SessionThing.getInstance().getDatabase().addPlaylist(p, null);
     		view.addPlaylist(p);
     	}else if(e.getActionCommand().equals("editList")){
-    		Playlist p = new Playlist(6, "Little Pony");
+    		Playlist p = new Playlist();
+    		p.setName("Gaga");
+    		SessionThing.getInstance().getDatabase().addPlaylist(p, null);
     		view.addPlaylist(p);
     	}
     	
