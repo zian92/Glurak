7ba909bef97ac0c7b0e0d4c9bacd34739e9d3146
Maxim <mxb.dance@gmx.de>
2014-03-09 11:34:55 +0100

    building follower View

---
+++ SProject/src/de/glurak/frontend/mainFrame/content/follower/FollowerVController.java
@@ -0,0 +1,73 @@
+package de.glurak.frontend.mainFrame.content.follower;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.List;
+import java.util.Observable;
+
+import javax.swing.JComponent;
+
+import de.glurak.data.Playlist;
+import de.glurak.data.User.User;
+import de.glurak.frontend.SessionThing;
+import de.glurak.frontend.mainFrame.ContentController;
+import de.glurak.frontend.mainFrame.NextContent;
+
+public class FollowerVController  extends Observable implements MouseListener, ActionListener, ContentController, NextContent {
+
+	private FollowerView view;
+	private ContentController nextContent;
+	 
+	public FollowerVController(){
+		this.view = new FollowerView(this, this);
+		view.setVisible(true);
+	    initfillView();
+	}
+	
+	public void initfillView(){
+		List<User> list = SessionThing.getInstance().getSessionUser().getFollowing();
+		view.fillView(list);
+	}
+	
+	public ContentController getNextContent() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public JComponent getView() {
+		return view;
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ SProject/src/de/glurak/frontend/mainFrame/content/follower/FollowerView.java
@@ -0,0 +1,68 @@
+package de.glurak.frontend.mainFrame.content.follower;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseListener;
+import java.util.List;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+import de.glurak.data.Playlist;
+import de.glurak.data.User.User;
+import de.glurak.feature.IconLoader;
+
+public class FollowerView extends JPanel {
+	private int favoCount = 0;
+	private JPanel pan_content;
+	private MouseListener mRef;
+	
+	public FollowerView(ActionListener a, MouseListener m){
+		super();
+		this.mRef = m;
+		this.setPreferredSize(new Dimension(600,500));
+		this.setLayout(new BorderLayout());
+		JScrollPane pan_scroll = new JScrollPane();
+		// TODO: Dynamisches wachsen bei hinzufügen
+		pan_content = new JPanel(new GridLayout(4,5));
+    	//parent.add(child, constr);
+	}
+	
+	public void addFollower(User favo){
+		JLabel icon = new JLabel(new IconLoader(200, 200, favo.getProfile().getPictureFileNameOrDefaultPictureName()).getIcon());
+		
+		icon.setForeground(Color.WHITE);
+    	icon.setFont(new Font("Verdana", Font.BOLD, 13));
+    	icon.setText(favo.getUsername());
+    	icon.setHorizontalTextPosition(JLabel.CENTER);
+    	icon.setVerticalTextPosition(JLabel.BOTTOM);
+    	icon.setPreferredSize(new Dimension(200	,200));
+    	icon.setVisible(true);
+    	icon.addMouseListener(mRef);
+		
+		pan_content.add(icon);
+		favoCount++;	
+		
+	}
+	
+    public void fillView(List<User> list){
+    	int pl_count = list.size();
+    	
+    	for (int i = 0; i < pl_count; i++){
+    		
+    		addFollower(list.get(i));
+    	}
+    }
+    
+    public void refreshView(List<User> list){
+    	addFollower(list.get(list.size()-1));
+    }
+	
+}
