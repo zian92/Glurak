6b186a4ac8de2b4a05521b22b8ba58255192f13a
Simon Wolter <simon-wolter@web.de>
2014-03-11 12:11:57 +0100

    Javadoc

--- SProject/src/de/glurak/frontend/mainFrame/ContentController.java
+++ SProject/src/de/glurak/frontend/mainFrame/ContentController.java
@@ -2,6 +2,10 @@ package de.glurak.frontend.mainFrame;
 
 import javax.swing.JComponent;
 
+/**
+ * Kontroller fuer die Contents
+ *
+ */
 public interface ContentController {
 
 	public JComponent getView();
